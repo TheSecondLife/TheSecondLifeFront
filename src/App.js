049a6fb6f2f1db77de2435b22468fcdb6b974a4a
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-06-13 22:26:56 +0900

    docs structure added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import logo from "./logo.svg";
-import "./App.css";
-
+import "./css/App.module.css";
+import Enter from "./component/Enter";
+import "./css/App.module.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>The Second Life First Repo!!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Enter />
         </div>
     );
 }
---
+++ src/component/Enter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../css/Enter.module.css"
+function Enter() {
+  return (
+    <div className='page'>
+        <div className='titleWrap'>
+            시니어의 일상이 새롭게 시작되는 곳, 
+            <br/>
+            The SecondLife
+        </div>
+
+        <div className='contentWrap'>
+            <div className='button login'>로그인</div>
+            <div className='button signin'>회원가입</div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Enter
