72cf0b7b9ad616da40e2c9363729bc7a5bf56b2e
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-06-15 10:39:31 +0900

    feat boardR

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BoardPage from "./BoardPage";
+
+function App() {
+    return (
+        <div className="App">
+            <BoardPage></BoardPage>
+        </div>
+    );
+}
+
+export default App;
