a4feea2e715fa3ad22f5737bdc029e54bb5d4426
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-06-15 14:39:59 +0900

    feat login added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/Login";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>The Second Life First Repo!!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </Router>
         </div>
     );
 }
