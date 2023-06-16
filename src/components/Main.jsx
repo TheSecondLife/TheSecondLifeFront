f2f5bbe9a6f001d88f677e4f1ee1a3e98370f6c0
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-06-17 01:18:12 +0900

    feat. login search font

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,17 +4,21 @@ import style from "../css/Main.module.css";
 
 export default function Main(){
   return(
-    <div className={`${style.page}`}>
-      <div style={{textAlign:'center', height:'70px', lineHeight: '70px'}}>Second Life LOGO</div>
-      {/* 메인 이미지  */}
-      <div className={`${style.mainImg}`}>
-        <img src={process.env.PUBLIC_URL + '/mainImg.jpeg'}/>
-      </div>
+    <div className={`${style.page, style.fadein}`}>
+      {/* 로고 */}
+      <div className={style.logo}>Second Life</div>
 
-      <div className={`${style.buttonLayout}`}>
-        <button type="button" class="btn btn-outline-primary">로그인</button>
-        <button type="button" class="btn btn-outline-primary">회원가입</button>
-      </div>
+      <button type="button" class="btn btn-warning"><b>카카오로 시작하기</b></button>
+     
+      {/* 메인 이미지  */}
+      {/* <div className={`${style.mainImg}`}>
+        <img src={process.env.PUBLIC_URL + '/mainImg.jpeg'}/>
+      </div> */}
+
+      {/* <div className={`${style.buttonLayout}`}>
+        <button type="button" class="btn btn-outline-light">로그인</button>
+        <button type="button" class="btn btn-outline-light">회원가입</button>
+      </div> */}
       
     </div>
   );
