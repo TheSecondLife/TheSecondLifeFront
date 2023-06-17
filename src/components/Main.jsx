3375dde14fc21ad71b0305aa4f4ee96cd462582a
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-06-17 09:40:43 +0900

    feat. main 1차 confirm 요청
    메인 페이지 1차로 그려보았는데, 컨펌해주세요~

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,21 +5,31 @@ import style from "../css/Main.module.css";
 export default function Main(){
   return(
     <div className={`${style.page, style.fadein}`}>
+
       {/* 로고 */}
-      <div className={style.logo}>Second Life</div>
-
-      <button type="button" class="btn btn-warning"><b>카카오로 시작하기</b></button>
+      <div className={style.logo}>We're your <b style={{color:"#a26ce9"}}>Second Life</b></div>
      
-      {/* 메인 이미지  */}
-      {/* <div className={`${style.mainImg}`}>
-        <img src={process.env.PUBLIC_URL + '/mainImg.jpeg'}/>
-      </div> */}
+      {/* auto playing */}
+      <div className={style.slider} id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div className={style.slider} class="carousel-item active">
+            <img style={{margin:"auto"}} src={process.env.PUBLIC_URL + '/mainImg1_1.png'} class="d-block w-50" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img style={{margin:"auto"}} src={process.env.PUBLIC_URL + '/mainImg1_2.png'} class="d-block w-50" alt="..."/>
+          </div>
+          <div class="carousel-item">
+            <img style={{margin:"auto"}} src={process.env.PUBLIC_URL + '/mainImg1_3.png'} class="d-block w-50" alt="..."/>
+          </div>
+        </div>
+      </div>
 
-      {/* <div className={`${style.buttonLayout}`}>
-        <button type="button" class="btn btn-outline-light">로그인</button>
-        <button type="button" class="btn btn-outline-light">회원가입</button>
-      </div> */}
+      {/* <img src={process.env.PUBLIC_URL + '/mainImg.jpeg'}/> */}
       
+
+      <div><button className={style.btn} style={{marginTop:"5%"}}>로그인</button></div>
+      <div><button className={style.btn} style={{marginTop:"5%"}}>회원가입</button></div>
+
     </div>
   );
 }
