b26071996cf0fcb1291060194380ed72e9d9896d
ShinSanha <33896511+SahhaShin@users.noreply.github.com>
2023-06-18 20:57:00 +0900

    feat: login ver 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import "./App.css";
 import { Route, Routes, Link, useNavigate, Outlet, Router } from 'react-router-dom';
 
 import BoardPage from "./BoardPage";
-import Home from "./Home";
-import Create from "./Create";
 import Login from "./pages/login/Login";
-import Main from "./pages/login/Main"; {/* 이거 지우기 */}
+import Main from "./pages/login/Main";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
-import Login from "./components/Login";
 import Detail from "./pages/Detail";
 
 function App() {
@@ -18,7 +15,7 @@ function App() {
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/" element={<Home></Home>}></Route>
                     <Route path="/boardCreate" element={<Create />}></Route>
-                    <Route path="/main" element={<Main />}></Route> {/* 이거 지우기 */}
+                    <Route path="/main" element={<Main />}></Route>
                     <Route path='/boardDetail' element={<Detail />} />
 
                 </Routes>
--- src/pages/login/Main.jsx
+++ src/pages/login/Main.jsx
@@ -4,20 +4,19 @@ import style from "../../css/Main.module.css";
 
 export default function Main(){
 
-  let [latitude, setLatitude] = useState(-1);
-  let [longitude, setLongitude] =useState(-1);
+  // let [latitude, setLatitude] = useState(-1);
+  // let [longitude, setLongitude] =useState(-1);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      setLatitude(pos.coords.latitude);
-      setLongitude(pos.coords.longitude);
-      const url = process.env.REACT_APP_KAKAO_LOGIN_URL;
-      // console.log(url)
-  });
-  }, [])
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(function(pos) {
+  //     setLatitude(pos.coords.latitude);
+  //     setLongitude(pos.coords.longitude);
+  //     const url = process.env.REACT_APP_KAKAO_LOGIN_URL;
+  // });
+  // }, [])
   return(
     <div className={`${style.page, style.fadein}`}>
-      <p>위도 : {latitude}, 경도 : {longitude}</p>
+      {/* <p>위도 : {latitude}, 경도 : {longitude}</p> */}
 
       {/* 로고 */}
       <div className={style.logo}>We're your <b style={{color:"#a26ce9"}}>Second Life</b></div>
