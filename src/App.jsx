1880134b7b4701474a93666a30772e00cf6328ff
minsung <jiminsung@naver.com>
2023-06-18 21:40:15 +0900

    feat : route 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import "./App.css";
 import { Route, Routes, Link, useNavigate, Outlet, Router } from 'react-router-dom';
 
-import BoardPage from "./BoardPage";
-import Login from "./pages/login/Login";
+// import BoardPage from "./BoardPage";
+// import Login from "./pages/login/Login";
 import Main from "./pages/login/Main";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Create from "./pages/Create";
 import Detail from "./pages/Detail";
+import KakaoCallback from './pages/login/KakaoCallback'
+import Select from './pages/Select';
+
 
 function App() {
     return (
         <div className="App">
                 <Routes>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/" element={<Home></Home>}></Route>
+                    <Route path="/" element={<Main />} />
+                    <Route path='/kakao/callback' element={<KakaoCallback></KakaoCallback>}> </Route>
+                    <Route path='/home' element={<Select/>}></Route>
+                    {/* <Route path="/" element={<Home></Home>}></Route> */}
                     <Route path="/boardCreate" element={<Create />}></Route>
                     <Route path="/main" element={<Main />}></Route>
                     <Route path='/boardDetail' element={<Detail />} />
