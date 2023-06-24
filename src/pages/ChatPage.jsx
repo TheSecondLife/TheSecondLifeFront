3a9fac548179c74cfa676e830e416f64bb15ffa8
minsung <jiminsung@naver.com>
2023-06-24 23:40:51 +0900

    feat : chat 데이터수정

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -7,53 +7,45 @@ import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import style from "../css/ChatPage.module.css"
-
-// redux
-import { useSelector, useDispatch } from 'react-redux';
-import {getChatList} from "../store/chatSlice";
+import { compose } from '@reduxjs/toolkit';
 
 function ChatPage() {
 
-  let state_chat = useSelector((state)=> state.chat);
-  let dispatch = useDispatch();
-
   const navigate = useNavigate();
 
-  // let [chatList, setChatList] = useState([]);
+  let [chatList, setChatList] = useState([]);
   let [chat, setChat] = useState('');
   let [otherName, setOtherName] = useState('');
   let [userId, setUserId] = useState(1);
-
   let [name, setName] = useState('');
   const profileImg = JSON.parse(sessionStorage.getItem("loginUser")).profileImg;
 
   const { roomId } = useParams();
   const { otherId } = useParams();
 
+
   const client = useRef({});
 
-  // 스크롤 최하단 위치 
   const scrollRef = useRef();
   useEffect(() => {
     scrollRef.current?.scrollIntoView({behavior : 'smooth'});
-  }, [state_chat.chatList])
+  }, [chatList])
 
 
   function connect() {
-    console.log(4);
     client.current = new StompJs.Client({
       brokerURL: process.env.REACT_APP_WS,
       onConnect: () => {
         console.log('success');
         subscribe();
-        console.log(7);
       },
     });
+    // console.log("connect")
+    // console.log(roomId)
     client.current.activate();
   };
 
   function publish(chat) {
-    console.log(8);
     if (!client.current.connected) {
       console.log(`no!!`);
       return;
@@ -65,29 +57,29 @@ function ChatPage() {
         userId: userId,
         nickname: name,
         profileImg: profileImg,
-        chat: chat,
+        content: chat,
       }),
     });
-
-    loadData(); //또 데이터 불러와 
-    setChat(''); //이게 변하면서 전체적으로 새롭게 대화 추가 후 재렌더링됨
+    setChat('');
   };
 
   function subscribe() {
-    console.log(5);
+    // console.log("subcribe")
+    // console.log(roomId)
     client.current.subscribe('/sub/chat/' + roomId, (body) => {
-      console.log(6);
+      console.log(`json_body : ${body.body}`);
       const json_body = JSON.parse(body.body);
+      
       const message = json_body;
+      // console.log("여기!");
+      console.log(message);
 
       // 이게 랜더링 이슈로인해서 ㅜㅜㅜㅜㅜㅜㅜㅜㅜ
-      // console.log(message);
-      // dispatch(getChatList(message)); //새로운 메세지 추가하여 새롭게 리스트 배치 
-      // console.log(`state_chat.chatList : ${state_chat.chatList}`);
-      // setChat('');
+      setChatList((_chat_list) => [
+        ..._chat_list, message
+      ]);
     });
   };
-
 
   function disconnect() {
     client.current.deactivate();
@@ -112,39 +104,43 @@ function ChatPage() {
     setUserId(event.target.value);
   }
 
+  
+  useEffect(() => {
 
+    // url 접근 막기
+    // let roomId2 = localStorage.getItem("roomId")
+    // if (roomId2 == null) {
+    //   navigate("/home")
+    //   return
+    // }
 
-  const loadData = async () => {
-    console.log(2);
-    let userId = JSON.parse(sessionStorage.getItem("loginUser")).id; //로그인 유저 가져오기 
+    let userId = JSON.parse(sessionStorage.getItem("loginUser")).id;
 
     const url = "/api/chat"
     const data = {
       userA: userId,
       userB: otherId
     }
-    const config = {"Content-Type": 'application/json'}; //데이터 헤더 json 형식 전송 
+    const config = {"Content-Type": 'application/json'};
     
-    let result_2 = await axios.post(url, data, config)
+    axios.post(url, data, config)
     .then((result) => {
-      // dispatch(getChatList(result.data)); //이거만 하면 소용 없음 
-      return result;
+      setOtherName(result.data.talkUserName);
+      setChatList(result.data.chatList);
+      // console.log(33)
+      // console.log(chatList) 
+    })
+    .then(() => {
+      // console.log(chatList)
+      connect();
+
+      // setUp(false);
     })
 
-    setOtherName(result_2.data.talkUserName);
-    dispatch(getChatList(result_2.data.chatList));
-  
     return () => {
       localStorage.removeItem("roomId")
       disconnect();
     }
-  }
-
-  useEffect(() => {
-    console.log(1);
-    loadData();
-    console.log(3);
-    connect();
   }, []);
 
 
@@ -170,9 +166,9 @@ function ChatPage() {
       <div>
         <br></br>
         <br></br>
-
         {/* temp */}
-        <div>
+        {/* <h4>chatRoom : {roomId}</h4> */}
+        {/* <div>
           name : {name}
           <InputGroup/>
           <InputGroup className="mb-3">
@@ -184,44 +180,42 @@ function ChatPage() {
             <Form.Control placeholder="" type={'text'} onChange={idChange} value={userId} />
           </InputGroup>
         </div>
-        <hr></hr>
+        <hr></hr> */}
         {/* temp */}
 
         {
-       
-            state_chat.chatList[0] && state_chat.chatList.map((item, index) => {
-              return (
-                <div key={index}>
-                  {
-                    userId == item.userId ? null
-                    :                   
-                    <div className={style.other}>
-                      <img className={style.chatProfileImg} src={item.profileImg}/>
-                      <p className={style.name}>{item.nickname}</p>
-                    </div> 
-                  }
-                  <div className={
-                    userId == item.userId ? style.right:style.left
-                  }>
-                    {item.content}
-                  </div>
-  
-                  <div ref={scrollRef}></div>
-  
-  
-                  {
-                    userId == item.userId?
-                    <div>
-                      <br></br><br></br>
-                    </div>
-                    :
-                    null
-                  }
+          chatList.map((item, index) => {
+            return (
+              <div key={index}>
+                {
+                  userId == item.userId ? null
+                  :                   
+                  <div className={style.other}>
+                    <img className={style.chatProfileImg} src={item.profileImg}/>
+                    <p className={style.name}>{item.nickname}</p>
+                  </div> 
+                }
+                <div className={
+                  userId == item.userId ? style.right:style.left
+                }>
+                  {item.content}
                 </div>
-              )
-            })
-          }
-        
+
+                <div ref={scrollRef}></div>
+
+
+                {
+                  userId == item.userId?
+                  <div>
+                    <br></br><br></br>
+                  </div>
+                  :
+                  null
+                }
+              </div>
+            )
+          })
+        }
         <br></br>
         <br></br>
         <br></br>
@@ -237,6 +231,9 @@ function ChatPage() {
         </div>
 
       </div>
+      {/* <div style={{height: "51px"}}>
+        <Footer />
+      </div> */}
     </>
   )
 }
